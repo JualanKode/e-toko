18f19dffd70047e8f5143c9f2609c269bcf440e0
JualanKode.com <jualankode.com@gmail.com>
2023-07-18 17:35:45 +0700

    komentar2

--- application/views/header.php
+++ application/views/header.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/summernote/summernote-bs4.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
-    <!-- DataTables -->
+    <!-- DataTabless -->
     <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
     <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
